e68460e20ea0ab7394bd1bf736bb3463883147e4
Kevin Wang <kev.wang424@gmail.com>
2017-04-18 21:31:07 -0400

    got api tokens into main.js

--- javascript/main.js
+++ javascript/main.js
@@ -3,24 +3,32 @@ $(handleClick)
 //define buttonHandler
 function handleClick() {
   var $button = $("#click-me")
-  $button.click(function(position){
+  $button.click(function(){
     $button.hide()
-    findAndRenderEvents(position)
+    getLocation()
+    findAndRenderEvents(coordinates)
   })
 }
 
 //search events by location of the user
-function findAndRenderEvents(position){
+function findAndRenderEvents(coordinates){
+  debugger
   const URL = "http://api.eventful.com/rest/events/search?...&where=32.746682,-117.162741"
   const auth = "&within=25&date=Future\&app_key="
-  debugger
-  findLocation()
 }
 
-//search users locations
-function findLocation(){
+//search the users locations
+function getLocation(){
   const URL = "https://www.googleapis.com/geolocation/v1/geolocate?key="
-  
+  $.ajax({
+    type: "POST",
+    url: `${URL}${apiKeys.google_geolocation_api_key}`,
+    success: getCoords
+  });
+}
+
+function getCoordsAndEvents(data){
+  debugger
 }
 
 // http://api.eventful.com/rest/events/search?...&where=32.746682,-117.162741&within=25&date=Future\&app_key=cmMZxgb87BQPf3cQ
